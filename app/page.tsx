ec67e187e83b9379b4a42290d22e88e8aff7316a
vishu <vishwajeetsinghchauhan63@gmail.com>
2024-02-12 15:20:16 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const canvasObjects = useStorage((root) => root.canvasObjects); */
   return (
     <main className="h-screen overflow-hidden">
       
-    <Navbar />
+    {/* <Navbar /> */}
 
     <section className="flex flex-row border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
   <LeftSidebar/>
