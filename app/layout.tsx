2bb38da1b4c52bfb81d807ecfca273162464f05a
vishu <vishwajeetsinghchauhan63@gmail.com>
2024-02-12 15:45:10 +0530

    project title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const workSans = Work_Sans({
  });
 
 export const metadata: Metadata = {
-  title: "Figma Clone",
+  title: "Figmabyvishu",
   description: "A design tool for teams who build products together.",
 };
 
