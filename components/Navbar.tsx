f4fbc9133f3d13633ef9553108f5e39f7fa75705
vishu <vishwajeetsinghchauhan63@gmail.com>
2024-02-12 15:27:25 +0530

    minor change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,16 @@
 "use client";
 import Image from "next/image";
 import { navElements } from "@/constants";
-import ShapesMenu from "./ShapesMenu";
 
-import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
     <nav className="flex select-none items-center justify-between gap-4 px-5 bg-primary-black  text-white">
       <Image src="/assets/logo.svg" alt="logo" width={100} height={100} />
       <ul className="flex flex-row">
-        {navElements.map((item) => (
-          
-          <li>
-          <ul>Home</ul>
-          <ul>Gallery</ul>
-          <ul>contact</ul>
-          </li>
-        ))}
+          <li>Home</li>
+          <li>components</li>
+          <li>gallery</li>
       </ul>
       <h1>Activeusers</h1>
     </nav>
